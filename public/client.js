36195b4f8d528b49adc09bc454fd0cad608ef7ca
System <none>
2017-02-22 12:08:00 +0000

    🌻🐛 Updated with Gomix

--- public/client.js
+++ public/client.js
@@ -14,10 +14,20 @@ Array.prototype.flatMap = function(f){
   .reduce( (p,n) => p.concat(n) )
 }
 
-function Move({ velocity:v, coords:p, id }){
+function Move({ velocity:v, coords:p, acceleration:a, id }){
 
     return [
       { action: 'UpdateComponent'
+      , component: { x: v.x + a.x, y: v.y + a.y }
+      , type: 'Velocity'
+      , id
+      }
+      ,{ action: 'UpdateComponent'
+      , component: { x: 0, y: 0 }
+      , type: 'Acceleration'
+      , id
+      }
+      ,{ action: 'UpdateComponent'
       , component: { x: p.x + v.x, y: p.y + v.y }
       , type: 'Coords'
       , id
@@ -25,7 +35,25 @@ function Move({ velocity:v, coords:p, id }){
     ]
 }
 
-function Frame( { RenderFlat={}, Coords={}, Dimensions={}, Velocity={} }){
+function AirDrag({ velocity:v, id}){
+  
+  return [
+    { action: 'UpdateComponent'
+    , component: { x: v.x, y: v.y }
+    , type: 'Velocity'
+    , id
+    }
+  ]
+}
+
+function Frame( { 
+  RenderFlat={}
+  , Coords={}
+  , Dimensions={}
+  , Velocity={}
+  , Acceleration={} 
+  , AirDrag={}
+}){
   
   return Object.keys( RenderFlat )
       .map(function(id){
@@ -42,8 +70,18 @@ function Frame( { RenderFlat={}, Coords={}, Dimensions={}, Velocity={} }){
 
             const velocity = Velocity[id]
             const coords = Coords[id]
+            const acceleration = Acceleration[id]
 
-            return { action: 'Move', coords, velocity, id }
+            return { action: 'Move', coords, velocity, acceleration, id }
+          })
+      )
+      .concat(    
+        Object.keys( AirDrag )
+          .map(function(id){
+
+            const velocity = Velocity[id]
+            
+            return { action: 'AirDrag', velocity, id }
           })
       )
 }
@@ -77,6 +115,13 @@ function update( state, action ){
         RenderFlat(action)
       ) 
     })
+  } else if ( action.action == 'AirDrag' ){
+    
+    return merge(state, {
+      actions: state.actions.concat(
+        AirDrag(action)
+      )
+    })
     
   } else if ( action.action == 'Move' ){
     
@@ -89,7 +134,7 @@ function update( state, action ){
   } else if (action.action == 'UpdateComponent'){
     
     const { component, type, id } = action
-    
+
     return merge(
       state
       ,{ [type]: 
@@ -119,6 +164,13 @@ name: 'Level 01'
     ,Coords: { x: x * 10, y: y * 10 }
     ,Acceleration: { x:0, y: 0 }
     ,Velocity: { x:0, y:0 }
+    ,AirDrag: {}
+    ,Input: {
+      Left: { Acceleration: { x: -1 } }
+      ,Right: { Acceleration: { x: 1 } }
+      ,Up: { Acceleration: { y: -1 } }
+      ,Down: { Acceleration: { y: 1 } }
+    }
   })
   
   ,'#': ({x,y,w,h,type}) => ({
@@ -144,8 +196,11 @@ name: 'Level 01'
 #   #                      ##
 #E  #                     ###
 #####                    ####
+
                   ####
+
            ####
+
      #####
 S    
 ###
@@ -217,12 +272,53 @@ function CanvasLoop(){
   const canvas = document.createElement('canvas')
   const context = canvas.getContext('2d')
   
+  const keys = {
+    Up: 0
+    ,Left: 0
+    ,Right: 0
+    ,Down: 0
+  }
+  
+  window.addEventListener('keydown', function(e){
+    const DIRECTION = 
+      [e.keyCode == 37 && 'Left'
+       , e.keyCode == 38 && 'Up'
+       , e.keyCode == 39 && 'Right'
+       , e.keyCode == 40 && 'Down'
+      ]
+      .find(Boolean)
+
+    if( DIRECTION ){
+      keys[DIRECTION] = keys[DIRECTION] || 0
+      keys[DIRECTION] += 1 
+    }    
+
+  })
+  
+  window.addEventListener('keyup', function(e){
+    const DIRECTION = 
+      [e.keyCode == 37 && 'Left'
+       , e.keyCode == 38 && 'Up'
+       , e.keyCode == 39 && 'Right'
+       , e.keyCode == 40 && 'Down'
+      ]
+      .find(Boolean)
+
+    if( DIRECTION ){
+      keys[DIRECTION] = keys[DIRECTION] || 0
+      keys[DIRECTION] = 0 
+    }
+    
+  })
+  
   function loop(){
     state.actions.push(
       { action: 'ClearScreen' }
       ,{ action: 'Frame' }
+      ,{ action: 'CheckKeyboardInput' }
     )
-
+    
+    
     var action;
     while ( action = state.actions.shift() ){
       state = update(state, action)
@@ -233,6 +329,27 @@ function CanvasLoop(){
       } else if( action.action == 'CanvasRender') {
         context.fillStyle = action.fillStyle 
         context[action.type].apply(context, action.args)
+      } else if( action.action == 'CheckKeyboardInput' ){
+        
+        Object.keys(keys)
+          .filter( direction => keys[direction] )
+          .forEach(function(direction){
+            
+            Object.keys(state.Input).forEach(function(entity){
+              
+              const input = state.Input[entity]
+              
+              if( input[direction] ){
+                 Object.keys( input[direction] )
+                  .forEach(function( ComponentType ){
+                     const original = state[ComponentType][entity] || {}
+                     const mixin = input[direction][ComponentType]
+                   
+                     state[ComponentType][entity] = Object.assign(original, mixin)
+                  })
+              }
+            })
+          })
       }
 
     }
